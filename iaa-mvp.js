9dad7b6761990ae0ce24cbbb6734f882b7929e98
Alan deLevie <adelevie@gmail.com>
2015-07-23 15:14:50 -0400

    Made state drop-downs dynamic

--- iaa-mvp.js
+++ iaa-mvp.js
@@ -50,6 +50,24 @@ if (Meteor.isClient) {
     */
     d: function(attr) {
       return this[attr];
+    },
+    usStates: function() {
+      return _.map(_.keys(USStates), function(abbrev) {
+        return {
+          abbrev: abbrev,
+          name: USStates[abbrev]
+        }
+      });
+    },
+    isStateSelected: function(v1, v2) {
+      var controller = Iron.controller();
+      var formValues = controller.state.get('formValues');
+      console.log("v1: "+ formValues[v1] + " | v2: " + v2);
+      if (formValues[v1] === v2) {
+        return 'selected';
+      } else {
+        return '';
+      };
     }
   });
 
