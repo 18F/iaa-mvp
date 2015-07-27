d3fd5cdbcc9bee45dc0c4a31070128868d2fffc6
Alan deLevie <adelevie@gmail.com>
2015-07-27 19:06:16 -0400

    Cleaning up console.log and some code comments

--- routes.js
+++ routes.js
@@ -8,15 +8,9 @@ Router.configure({
 });
 
 Router.onBeforeAction(function () {
-  // all properties available in the route function
-  // are also available here such as this.params
-
   if (!Meteor.userId()) {
-    // if the user is not logged in, render the Login template
     this.render('landing_page');
   } else {
-    // otherwise don't hold up the rest of hooks or our route/action function
-    // from running
     this.next();
   }
 });
@@ -35,7 +29,6 @@ Form7600AController = ApplicationController.extend({
 IndexController = ApplicationController.extend({
   action: function() {
     var forms = Form7600A.find().fetch();
-    console.log(forms);
     this.state.set('forms', forms);
     this.render();
   }
