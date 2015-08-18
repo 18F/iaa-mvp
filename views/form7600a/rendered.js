0b485bb69f44f376831ba47aa94ae441a2a8f8e0
Andrew Maier <andrew.maier@gsa.gov>
2015-08-18 14:55:37 -0400

    Update styles per conversation with Alan

--- views/form7600a/rendered.js
+++ views/form7600a/rendered.js
@@ -25,8 +25,6 @@ if (Meteor.isClient) {
 
     $('.field input, .field textarea, .field select').focus(function(){ $(this).parents('.field').addClass('is-focused'); });
     $('.field input, .field textarea, .field select').blur(function(){ $(this).parents('.field').removeClass('is-focused'); });
-
-
   });
 }
 
