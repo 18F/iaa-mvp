e4f1876795a2426ad8c0a08fc176d2f9469dee1d
Andrew Maier <andrew.maier@gsa.gov>
2015-08-18 14:57:09 -0400

    Move form-field helper jquery to rendered.js

--- client/javascripts/application.js
+++ client/javascripts/application.js
@@ -15,7 +15,4 @@ $(document).ready(function(){
     });
   });
 
-  $('.field input, .field textarea, .field select').focus(function(){ $(this).parents('.field').addClass('is-focused'); });
-  $('.field input, .field textarea, .field select').blur(function(){ $(this).parents('.field').removeClass('is-focused'); });
-
 });
--- views/form7600a/rendered.js
+++ views/form7600a/rendered.js
@@ -23,6 +23,10 @@ if (Meteor.isClient) {
       $('.form-7600a').submit();
     });
 
+    $('.field input, .field textarea, .field select').focus(function(){ $(this).parents('.field').addClass('is-focused'); });
+    $('.field input, .field textarea, .field select').blur(function(){ $(this).parents('.field').removeClass('is-focused'); });
+
+
   });
 }
 
