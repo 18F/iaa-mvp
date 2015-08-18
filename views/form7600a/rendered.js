f82c6d8c3c8eb5446edb625f6b4258839570a671
Andrew Maier <andrew.maier@gsa.gov>
2015-08-18 14:55:37 -0400

    Update styles per conversation with Alan

--- views/form7600a/rendered.js
+++ views/form7600a/rendered.js
@@ -1,5 +1,6 @@
 if (Meteor.isClient) {
   Template.form_7600a.onRendered(function() {
+    $('main').addClass('l-7600a-new');
     // set autosave timer
     // Disabling per bug report from Eric Mill
     // var timer =  setInterval(submitForm7600A, 10000);
@@ -21,6 +22,7 @@ if (Meteor.isClient) {
       }
       $('.form-7600a').submit();
     });
+
   });
 }
 
