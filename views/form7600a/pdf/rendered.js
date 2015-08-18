66e7158d1ac1f8113e6c53507be4e24c5190080c
Alan deLevie <adelevie@gmail.com>
2015-08-17 22:50:27 -0400

    pdfjs proof of concept

--- views/form7600a/pdf/rendered.js
+++ views/form7600a/pdf/rendered.js
@@ -1,11 +1,37 @@
-if (Meteor.isClient) {
+if (Meteor.isClient) {  
   Template.form_7600a_pdf.onRendered(function() {
-    Session.set('downloadPrompt', 'Your download will begin shortly.')
     var controller = Iron.controller();    
     var formId = controller.state.get('formId');   
     var form = Form7600A.findOne({_id: formId});
     
-    DownloadForm7600A(form);
-    Session.set('downloadPrompt', 'Your form has downloaded. You can refresh the page to download again.');
+    var url = 'https://iaa-pdf-api.18f.gov/iaa/7600a';
+    var options = {
+      data: TransformForm7600AToPDFAttributes(form),
+      responseType: 'blob'
+    };
+
+    var callback = function(error, result) {
+      if (!error) {
+        var blob = result.content;
+        var reader = new FileReader();
+        reader.readAsDataURL(blob);
+        reader.onloadend = function() {
+          var dataUri = reader.result;
+          var binaryDataUri = ConvertDataURIToBinary(dataUri);
+          
+          PDFJS.workerSrc = '/packages/pascoual_pdfjs/build/pdf.worker.js';
+          PDFJS.getDocument(url).then(function getPdfHelloWorld(pdf) {            
+              RenderPdfPage(pdf, 1, 'pdfcanvas1');
+              RenderPdfPage(pdf, 2, 'pdfcanvas2');
+              RenderPdfPage(pdf, 3, 'pdfcanvas3');
+              RenderPdfPage(pdf, 4, 'pdfcanvas4');
+          });       
+        };    
+      } else {
+        alert('There was an error downloading the PDF: ' + error);
+      }
+    };
+
+    HTTP.post(url, options, callback);
   });
 }
