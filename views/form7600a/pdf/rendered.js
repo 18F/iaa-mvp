abd848f8fde7cb54b1adc3aedde2692ea63ecf2a
Alan deLevie <adelevie@gmail.com>
2015-08-19 17:40:49 -0400

    Added pdf rendering to autorun callback

--- views/form7600a/pdf/rendered.js
+++ views/form7600a/pdf/rendered.js
@@ -1,32 +1,29 @@
 if (Meteor.isClient) {  
   Template.form_7600a_pdf.onRendered(function() {
-    
-    var controller = Iron.controller();    
-    var formId = controller.state.get('formId');   
-    var form = Form7600A.findOne({_id: formId});
-    
-    var url = 'https://iaa-pdf-api.18f.gov/iaa/7600a';
-    var options = {
-      data: TransformForm7600AToPDFAttributes(form),
-      responseType: 'blob'
-    };
-
-    var callback = function(error, result) {
-      if (!error) {
-        var blob = result.content;
-        var canvases = ['pdfcanvas1', 'pdfcanvas2', 'pdfcanvas3', 'pdfcanvas4'];
-        Render7600APDFFromBlob(blob, canvases);
-      } else {
-        alert('There was an error rendering the PDF: ' + error);
-      }
-    };
-
-    HTTP.post(url, options, callback);
-    
     var self = this;
     
     self.autorun(function() {
-      
+      var controller = Iron.controller();    
+      var formId = controller.state.get('formId');   
+      var form = Form7600A.findOne({_id: formId});
+    
+      var url = 'https://iaa-pdf-api.18f.gov/iaa/7600a';
+      var options = {
+        data: TransformForm7600AToPDFAttributes(form),
+        responseType: 'blob'
+      };
+
+      var callback = function(error, result) {
+        if (!error) {
+          var blob = result.content;
+          var canvases = ['pdfcanvas1', 'pdfcanvas2', 'pdfcanvas3', 'pdfcanvas4'];
+          Render7600APDFFromBlob(blob, canvases);
+        } else {
+          alert('There was an error rendering the PDF: ' + error);
+        }
+      };
+
+      HTTP.post(url, options, callback);
     });
   });
 }
