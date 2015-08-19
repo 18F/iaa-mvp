a5efd8cef712582fee586ae3dc038a42686b44c3
Alan deLevie <adelevie@gmail.com>
2015-08-19 13:52:36 -0400

    PDFJS and filesaverjs, working on different routes

---
+++ views/form7600a/download/rendered.js
@@ -0,0 +1,11 @@
+if (Meteor.isClient) {  
+  Template.form_7600a_pdf_download.onRendered(function() {
+    Session.set('downloadPrompt', 'Your download will begin shortly.')
+    var controller = Iron.controller();    
+    var formId = controller.state.get('formId');   
+    var form = Form7600A.findOne({_id: formId});
+    
+    DownloadForm7600A(form);
+    Session.set('downloadPrompt', 'Your form has downloaded. You can refresh the page to download again.'); 
+  });
+}
