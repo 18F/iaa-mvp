f06b17330f6e38153fb3e2b91faaef25ce471225
Brandon Kirby <brandon.kirby@gsa.gov>
2015-09-02 17:19:27 -0400

    Migrate "legacy" user and form7600a data to add the default org. Will need to drop later to ensure we don't accidentally mess up the data later.

---
+++ config/server/data_migration.js
@@ -0,0 +1,6 @@
+Meteor.startup(function () {
+  Meteor.users.update({"orgs": {$exists: false}}, {$set: {orgs: ["18F"]}}, {multi: true});
+  Form7600A.update({"org": {$exists: false}}, {$set: {org: "18F"}}, {multi: true});
+});
+
+
